433e5d9d22bca9390799c4406492a6bc446fc29b
danielMut02 <dmut5502@gmail.com>
2025-11-26 09:51:02 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }   
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    // if the received data has an empty content property, respond with: 400 bad request
+
+    if (!body.content) {
+      return response.status(400).json({
+        error: "content missing"
+      })
+    }
+
+    const note = {
+      content: body.content,
+      // if the important property is missing, set a default of false
+      important: body.important || false,
+      id: generateId()
+    }
+
+    console.log(note)
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
